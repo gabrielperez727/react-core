a0a6eae8fb11c8c8e3a58d56b9561110c1ad3c5d
Gabriel Perez <gabrielrperez.dev@gmail.com>
2024-05-29 05:45:15 -0400

    Add default prop for new react components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <Student name="Spongebob" age={22} isStudent={true} />
       <Student name="Patrick" age={42} isStudent={false} />
       <Student name="Planktin" age={32} isStudent={false} />
+      <Student />
     </>
   );
 }
--- my-react-app/src/Students.jsx
+++ my-react-app/src/Students.jsx
@@ -1,4 +1,4 @@
-import PropTypes from "./prop-types";
+import PropTypes from "prop-types";
 
 function Student(props) {
   return (
@@ -16,5 +16,11 @@ Student.propTypes = {
   name: PropTypes.string,
   age: PropTypes.number,
   isStudent: PropTypes.bool,
+};
+
+Student.defaultProps = {
+  name: "Guest",
+  age: 0,
+  isStudent: false,
 };
 export default Student;
